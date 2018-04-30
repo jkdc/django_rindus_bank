952de4ac412cb1a465a19815bad0fc3974fd40b7
Joaquin <joaquincardosobuzon@gmail.com>
2018-04-30 23:19:47 +0000

    models and django-localflavor added

--- bank/models.py
+++ bank/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
+from localflavor.generic.models import IBANField
 
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    admin_person = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __unicode__(self):
+        return self.first_name
+
+class Account(models.Model):
+    iban = IBANField()
+    person = models.ForeignKey(Person, on_delete=models.CASCADE,)
+
+    def __unicode__(self):
+        return self.iban

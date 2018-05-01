edb2759426fe19ae17e7398089af41a73a5abd9f
Joaquin <joaquincardosobuzon@gmail.com>
2018-05-01 00:25:41 +0000

    update home view and template, bug solved (store admin_person) in person_create view

--- bank/views.py
+++ bank/views.py
@@ -3,18 +3,22 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 from bank.forms import PersonForm
+from bank.models import Person
 
 
 @login_required
 def home(request):
-    return render(request, 'home.html')
+    if request.user.is_authenticated:
+        accounts = Person.objects.filter(admin_person=request.user.pk)
+        return render(request, 'home.html', {'people_list': accounts})
+    return redirect('login')
 
 @login_required
 def person_create(request, template_name='crud/create.html'):
     form = PersonForm(request.POST or None)
     if form.is_valid():
         person = form.save(commit=False)
-        person.user = request.user
+        person.admin_person = request.user
         person.save()
         return redirect('home')
     return render(request, template_name, {'form':form})
